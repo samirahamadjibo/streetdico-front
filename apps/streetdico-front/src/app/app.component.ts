4a2177e3cc3d1918be40a0b62b062350e67a36e3
samirahamadjibo <samira.hamadjibo@gmail.com>
2022-09-05 14:53:12 +0200

    initial commit

---
+++ apps/streetdico-front/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'digitalvitae-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'streetdico-front';
+}
