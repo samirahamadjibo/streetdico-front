e87990bd35a44e1306444b102679333666edc772
samirahamadjibo <samira.hamadjibo@gmail.com>
2022-09-08 01:21:49 +0200

    home header searchbar

--- apps/streetdico-front/src/app/app.module.ts
+++ apps/streetdico-front/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { MatIconModule } from '@angular/material/icon';
 
 import { AppComponent } from './app.component';
 import { WordComponent } from './components/shared/units/word/word.component';
@@ -16,7 +17,7 @@ import { HeaderHomeComponent } from './components/shared/core/header-home/header
     HeaderSimpleComponent,
     HeaderHomeComponent,
   ],
-  imports: [BrowserModule, HttpClientModule],
+  imports: [BrowserModule, HttpClientModule, MatIconModule],
   providers: [],
   bootstrap: [AppComponent],
 })
