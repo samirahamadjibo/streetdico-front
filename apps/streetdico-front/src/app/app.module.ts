fc16c0a6f562907758c527351e2e6827e572383b
samirahamadjibo <samira.hamadjibo@gmail.com>
2022-09-07 20:24:38 +0200

    configured styles

--- apps/streetdico-front/src/app/app.module.ts
+++ apps/streetdico-front/src/app/app.module.ts
@@ -5,9 +5,17 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { WordComponent } from './components/shared/units/word/word.component';
 import { HomeComponent } from './components/home/home.component';
+import { HeaderSimpleComponent } from './components/shared/core/header-simple/header-simple.component';
+import { HeaderHomeComponent } from './components/shared/core/header-home/header-home.component';
 
 @NgModule({
-  declarations: [AppComponent, WordComponent, HomeComponent],
+  declarations: [
+    AppComponent,
+    WordComponent,
+    HomeComponent,
+    HeaderSimpleComponent,
+    HeaderHomeComponent,
+  ],
   imports: [BrowserModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ apps/streetdico-front/src/app/components/shared/core/header-simple/header-simple.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'digitalvitae-header-simple',
+  templateUrl: './header-simple.component.html',
+  styleUrls: ['./header-simple.component.scss'],
+})
+export class HeaderSimpleComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
