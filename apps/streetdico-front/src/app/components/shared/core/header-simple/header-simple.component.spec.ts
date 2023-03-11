479356eb56d4f67996437fd32be5cdc16ca96c9a
Samira Hama Djibo <samira.hamadjibo@gmail.com>
2023-03-11 11:41:28 +0000

    Feature v1 header footer

---
+++ apps/streetdico-front/src/app/components/shared/core/header-simple/header-simple.component.spec.ts
@@ -0,0 +1,22 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { HeaderSimpleComponent } from './header-simple.component';
+
+describe('HeaderSimpleComponent', () => {
+  let component: HeaderSimpleComponent;
+  let fixture: ComponentFixture<HeaderSimpleComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [HeaderSimpleComponent],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(HeaderSimpleComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
