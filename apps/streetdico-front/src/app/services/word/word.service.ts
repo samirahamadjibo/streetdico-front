751cb21469de0173f8865e0f6acafa5df83fe9e3
samirahamadjibo <samira.hamadjibo@gmail.com>
2023-05-01 16:30:08 +0200

    ci_cd workflow

--- apps/streetdico-front/src/app/services/word/word.service.ts
+++ apps/streetdico-front/src/app/services/word/word.service.ts
@@ -8,7 +8,7 @@ import { HttpClient} from '@angular/common/http';
 export class WordService {
 
   constructor(private http: HttpClient) { }
-  private wordUrl = "http://localhost:8080/api/v1/words/trending"
+  private wordUrl = "ec2-3-93-147-97.compute-1.amazonaws.com:8080/api/v1/words"
 
   getTrendingWords() {
     return this.http.get<Word[]>(this.wordUrl);
