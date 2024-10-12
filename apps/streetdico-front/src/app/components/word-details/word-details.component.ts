21dd1f42bd311d8a555329a2031b507252d7362e
samirahamadjibo <samira.hamadjibo@gmail.com>
2024-10-12 13:13:44 +0200

    implemented skeleton for word details

--- apps/streetdico-front/src/app/components/word-details/word-details.component.ts
+++ apps/streetdico-front/src/app/components/word-details/word-details.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActiveWordsService } from '../../services/active-words/active-words.service';
 import { Word } from '../../models/word';
-import { finalize } from 'rxjs';
+import { finalize, tap } from 'rxjs';
 
 @Component({
   selector: 'digitalvitae-word-details',
@@ -18,11 +18,20 @@ export class WordDetailsComponent implements OnInit{
   public activeWords: Word[];
   public title: string;
   public isThereResult: boolean|undefined;
-  public isWordLoading= true;
+  public isWordLoading = true;
+  public skeletonWords: Word[] = [];  
 
   ngOnInit(): void {
+    this.setSkeletonWords()
     this.getSearchText();
     this.getActiveWords();
+  }
+
+  setSkeletonWords(){
+    this.skeletonWords.push(new Word)
+    this.skeletonWords.push(new Word)
+    this.skeletonWords.push(new Word)
+    this.activeWords = this.skeletonWords
   }
 
   getSearchText(){
@@ -33,14 +42,18 @@ export class WordDetailsComponent implements OnInit{
 
   getActiveWords(){
     this.activeWordService.getActiveWords().pipe(
+      tap(() => {
+        this.isWordLoading = false; 
+      }),
       finalize(() => {
-        this.isWordLoading = true;
+        this.isWordLoading = false;
         window.scrollTo(0, 0);
-      })
+      }),
     ).subscribe((words: Word[]) => {
       this.activeWords = words;
       this.isThereResult = this.activeWords.length != 0;
-      this.isWordLoading = false;
+      console.log(this.activeWords);
+      
     });    
   }
 }
